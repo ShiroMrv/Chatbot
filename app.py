9233564ee0ba358803e0d82d0f399d68f63adfca
Mariano Vallejos <82482751+ShiroMrv@users.noreply.github.com>
2026-01-04 16:04:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def get_response():
         return jsonify({"reply": f"Error de Dante: {str(e)}"}), 500
 
 if __name__ == '__main__':
-port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
